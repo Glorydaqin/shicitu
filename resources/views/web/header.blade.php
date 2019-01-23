5630fd4442a4697557ca4d4340b8d79485420ec8
YouNOFish <353027520@qq.com>
2019-01-23 14:45:10 +0800

    加入web app样式
    加入web app样式

--- resources/views/web/header.blade.php
+++ resources/views/web/header.blade.php
@@ -1,7 +1,10 @@
 <header class="head">
-  <a href="https://www.shicitu.com"><i class="icon"></i><span class="title">诗词兔</span></a><div class="nav-box"><a href="#" class="nav-item nav-item-active">推荐</a><a href="/list" class="nav-item">诗文</a><a href="/list" class="nav-item">作者</a></div>
+  <div class="title-box">
+    <a href="https://www.shicitu.com"><i class="icon"></i><span class="title">诗词兔</span></a>
+  </div>
+  <div class="nav-box"><a href="/" class="nav-item nav-item-active">推荐</a><a href="/list" class="nav-item">诗文</a><a href="/list" class="nav-item">作者</a></div>
   <div class="search-box">
-    <input type="text" class="search-ipt" placeholder="查询诗文、作者" >
+    <input type="text" class="search-ipt" placeholder="查询诗文、作者" maxlength="10" >
     <i class="icon"></i>
   </div>
 </header>
