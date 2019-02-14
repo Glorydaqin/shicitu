008d7ef8fed0b3592ba4636f6a232c6fcfdddabd
YouNOFish <353027520@qq.com>
2019-02-14 09:56:59 +0800

    修改详情样式
    修改详情样式

--- resources/views/web/info.blade.php
+++ resources/views/web/info.blade.php
@@ -4,13 +4,13 @@
     <div class="title-box">
         <span class="title title-line">{{ $item['title'] }}</span>
         <span class="title">-</span><span class="title">唐朝：{{ $item['author'] }}</span>
-
-        <div class="poetry-item">
-
-            @php($content = json_decode($item['paragraphs'],true))
-            @foreach($content as $value)
-                <p class="content">{{ $value }}</p>
-            @endforeach
-        </div>
     </div>
+    <div class="poetry-item">
+
+        @php($content = json_decode($item['paragraphs'],true))
+        @foreach($content as $value)
+            <p class="content">{{ $value }}</p>
+        @endforeach
+    </div>
+   
 @endsection
