550ee1d7173069e51939294087ea218fe985bfaf
YouNOFish <353027520@qq.com>
2019-02-26 10:12:00 +0800

    详情页面增加赞赏码
    详情页面增加赞赏码

--- resources/views/web/info.blade.php
+++ resources/views/web/info.blade.php
@@ -4,16 +4,26 @@
 @endsection
 
 @section("container")
-    <div class="title-box">
-        <span class="title title-line">{{ $item['title'] }}</span>
-        <span class="title">-</span><span class="title">唐朝：{{ $item['author'] }}</span>
+    <div class="left-content">
+        <div class="title-box">
+            <span class="title title-line">{{ $item['title'] }}</span>
+            <span class="title">-</span><span class="title">唐朝：{{ $item['author'] }}</span>
+        </div>
+        <div class="poetry-item">
+            @php($content = json_decode($item['paragraphs'],true))
+            @foreach($content as $value)
+                <p class="content">{{ $value }}</p>
+            @endforeach
+        </div>
     </div>
-    <div class="poetry-item">
+    <div class="right-content">
+        <img src="/web/img/zan.jpg" alt="赞赏码" class="qr-code">
+        <img src="/web/img/zan2.jpg" alt="赞赏码" class="qr-code">
+        <img src="/web/img/mini.jpg" alt="小程序二维码" class="qr-code">
+        <div class="text-box">
+            <p class="text">诗词兔</p>
+            <p class="text">关注学习更多诗词</p>
+        </div>
+    </div>
 
-        @php($content = json_decode($item['paragraphs'],true))
-        @foreach($content as $value)
-            <p class="content">{{ $value }}</p>
-        @endforeach
-    </div>
-   
 @endsection
