f024771eb67c6645eb5fdd98e0dc1e578d8d3444
秦维 <wei_qin@loongjoy.com>
2019-03-18 10:02:45 +0800

    save

--- app/Http/Controllers/Web/IndexController.php
+++ app/Http/Controllers/Web/IndexController.php
@@ -27,6 +27,17 @@ class IndexController extends Controller
 
     public function info($id){
         $item = Poetry::where("id",$id)->first();
+
+        //抓取数据,补全解释
+        $this->curlSave($item);
         return view('web.info',compact('item'));
+    }
+
+    private function curlSave($info){
+        $listUrl = "https://gushi.weixinshop.cc/api.php/ApiSearch/index?s_keyword=".urlencode($info['title'])."&utoken=4d6a073ec14d2b00af435b8a2a0a116d";
+
+        $id = '';
+        $contentUrl = 'https://gushi.weixinshop.cc/api.php/ShiApi/info?id='.$id.'&utoken=4d6a073ec14d2b00af435b8a2a0a116d&tdsourcetag=s_pctim_aiomsg';
+        dd($listUrl);
     }
 }
